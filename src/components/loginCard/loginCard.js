715c0a059764a4cfe334930076004edc74c4bc4f
Jonas Rodrigues <jonasrodrigues0312@gmail.com>
2023-09-22 10:14:19 -0300

    Enviando projeto

---
+++ src/components/loginCard/loginCard.js
@@ -0,0 +1,10 @@
+import styles from './loginCard.module.css'
+
+export default function LoginCard({title, children}) {
+    return (
+        <div className={styles.card}>
+            <h1 className={styles.title}>{title}</h1>
+            {children}
+        </div>
+    )
+}
---
+++ src/pages/login.js
@@ -0,0 +1,21 @@
+import styles from '../styles/Login.module.css'
+import Link from 'next/link'
+
+import LoginCard from "@/components/loginCard/loginCard"
+import Input from '@/components/input/input'
+import Button from '../components/button/button'
+
+export default function LoginPage() {
+    return (
+        <div className={styles.background}>
+            <LoginCard title={"Entre na sua conta"}>
+                <form className={styles.form}>
+                    <Input type="email" placeholder="Seu e-mail"/>
+                    <Input type="password" placeholder="Sua senha"/>
+                    <Button>Entrar</Button>
+                    <Link href="/cadastro">Ainda não possui conta?</Link>
+                </form>
+            </LoginCard>
+        </div>
+    )
+}
