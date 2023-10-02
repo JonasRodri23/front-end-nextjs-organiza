4ff84929be3f493fc49c069214a70fd7ef853c6f
Arthur Victor <arthurvspinheiro@gmail.com>
2023-10-02 11:01:34 -0300

    atualização da página investimentos

--- src/pages/investimentos.js
+++ src/pages/investimentos.js
@@ -1,28 +1,28 @@
 import React, { useState } from 'react';
-import Card from '../components/card/Card';
-import styles from '../components/card/Card.module.css';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 
 const Investimentos = () => {
   const [cards, setCards] = useState([]);
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-  const [value, setValue] = useState(''); // Novo estado para o valor
-  const [earnings, setEarnings] = useState(''); // Novo estado para os rendimentos
-  const [editingIndex, setEditingIndex] = useState(-1); // Estado para rastrear o card em edição
+  const [value, setValue] = useState('');
+  const [earnings, setEarnings] = useState('');
+  const [editingIndex, setEditingIndex] = useState(-1);
 
   const handleAddCard = () => {
     if (editingIndex === -1) {
-      // Adicionar um novo card
       const newCard = {
         title,
         description,
         value,
         earnings,
       };
-
       setCards([...cards, newCard]);
     } else {
-      // Atualizar um card existente
       const updatedCards = [...cards];
       updatedCards[editingIndex] = {
         title,
@@ -31,10 +31,9 @@ const Investimentos = () => {
         earnings,
       };
       setCards(updatedCards);
-      setEditingIndex(-1); // Limpar o índice de edição
+      setEditingIndex(-1);
     }
 
-    // Limpar os campos após adicionar ou atualizar o card
     setTitle('');
     setDescription('');
     setValue('');
@@ -48,7 +47,6 @@ const Investimentos = () => {
   };
 
   const handleEditCard = (index) => {
-    // Preencher os campos de edição com os valores do card selecionado
     const selectedCard = cards[index];
     setTitle(selectedCard.title);
     setDescription(selectedCard.description);
@@ -58,79 +56,69 @@ const Investimentos = () => {
   };
 
   return (
-    <div className={styles.cardPage}>
-      <div className={styles.cardForm}>
-        {editingIndex === -1 ? (
-          <>
-            <input
-              type="text"
-              placeholder="Title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="Description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="Value"
-              value={value}
-              onChange={(e) => setValue(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="Earnings"
-              value={earnings}
-              onChange={(e) => setEarnings(e.target.value)}
-            />
-            <button onClick={handleAddCard}>Add Card</button>
-          </>
-        ) : (
-          <>
-            <input
-              type="text"
-              placeholder="Edit Title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="Edit Description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="Edit Value"
-              value={value}
-              onChange={(e) => setValue(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="Edit Earnings"
-              value={earnings}
-              onChange={(e) => setEarnings(e.target.value)}
-            />
-            <button onClick={handleAddCard}>Update Card</button>
-            <button onClick={() => setEditingIndex(-1)}>Cancel</button>
-          </>
-        )}
-      </div>
+    <div style={{ padding: 20 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            fullWidth
+            label="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            fullWidth
+            label="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            fullWidth
+            label="Value"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            fullWidth
+            label="Earnings"
+            value={earnings}
+            onChange={(e) => setEarnings(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant="contained" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={handleAddCard}>
+            {editingIndex === -1 ? 'Add Card' : 'Update Card'}
+          </Button>
+          {editingIndex !== -1 && (
+            <Button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" onClick={() => setEditingIndex(-1)} style={{ marginLeft: 10 }}>
+              Cancel
+            </Button>
+          )}
+        </Grid>
+      </Grid>
 
-      <div className={styles.cardList}>
+      <div style={{ marginTop: 20 }}>
         {cards.map((card, index) => (
-          <div key={index}>
-            <Card
-              title={card.title}
-              description={card.description}
-              value={card.value}
-              earnings={card.earnings}
-              onRemove={() => handleRemoveCard(index)} // Passa a função de remoção
-              onEdit={() => handleEditCard(index)} // Passa a função de edição
-            />
+          <div key={index} style={{ marginBottom: 10 }}>
+            <Card>
+              <CardContent>
+                <h2>{card.title}</h2>
+                <p>{card.description}</p>
+                <p>Value: {card.value}</p>
+                <p>Earnings: {card.earnings}</p>
+                <Button variant="contained" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleEditCard(index)}>
+                  Edit
+                </Button>
+                <Button variant="contained" className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleRemoveCard(index)} style={{ marginLeft: 10 }}>
+                  Remove
+                </Button>
+              </CardContent>
+            </Card>
           </div>
         ))}
       </div>
